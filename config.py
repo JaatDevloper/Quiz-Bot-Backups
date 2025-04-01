ff8a8610e043b89a67676ccbf1e64debc57d98bc
JaatDevloper <pinterestjat2022@gmail.com>
2025-04-01 20:06:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ import os
 
 # Bot configuration
 TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN", "7867071540:AAF7T8I0vPgvFPVT7vb0v8sMIVYLKeH41-0")
-API_ID = os.environ.get("API_ID", "")  # Your Telegram API ID
-API_HASH = os.environ.get("API_HASH", "")  # Your Telegram API Hash
+API_ID = os.environ.get("API_ID", "28624690")  # Your Telegram API ID
+API_HASH = os.environ.get("API_HASH", "67e6593b5a9b5ab20b11ccef6700af5b")  # Your Telegram API Hash
 OWNER_ID = os.environ.get("OWNER_ID", "7656415064")  # Telegram ID of the bot owner
 
 DEFAULT_QUIZ_TIME = 60  # Default time limit for each question in seconds
